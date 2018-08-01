84142e74cf3ff85502f1b6a596962857aade1660
karnehen <karnehen@gmail.com>
2018-08-01 12:21:16 +0700

    8_seed_color: added filtering during seed dataset generation - improved seed recognition

--- src/seedcounter/examples/SeedDataset.java
+++ src/seedcounter/examples/SeedDataset.java
@@ -10,9 +10,6 @@ import org.opencv.imgproc.Imgproc;
 import seedcounter.colorchecker.ColorChecker;
 import seedcounter.colorchecker.FindColorChecker;
 import seedcounter.colorchecker.MatchingModel;
-import seedcounter.colormetric.ColorMetric;
-import seedcounter.colormetric.EuclideanLab;
-import seedcounter.colormetric.EuclideanRGB;
 import seedcounter.common.Helper;
 import seedcounter.common.Quad;
 import seedcounter.regression.ColorSpace;
@@ -56,10 +53,11 @@ class SeedDataset {
         writer.println(builder.toString());
     }
 
-    private static void printSingleSeed(MatOfPoint contour, Mat image, Mat seedBuffer,
-            PrintWriter writer, Map<String, String> data) {
+    private static List<Map<String,String>> getSeedData(MatOfPoint contour, Mat image, Mat seedBuffer,
+                                                        double threshold) {
         Imgproc.drawContours(seedBuffer, Collections.singletonList(contour), 0,
                 new Scalar(255.0), Core.FILLED);
+        List<Map<String,String>> result = new ArrayList<>();
 
         int minX = image.cols() - 1;
         int maxX = 0;
@@ -86,18 +84,35 @@ class SeedDataset {
                 if (seedBuffer.get(y, x)[0] > 0.0) {
                     double[] color = image.get(y, x);
                     if (color[0] + color[1] + color[2] > 0.0) {
-                        data.put("x", String.valueOf(x));
-                        data.put("y", String.valueOf(y));
-                        data.put("blue", String.valueOf(color[0]));
-                        data.put("green", String.valueOf(color[1]));
-                        data.put("red", String.valueOf(color[2]));
-                        printMap(writer, data);
+                        Map<String,String> map = new HashMap<>();
+                        map.put("x", String.valueOf(x));
+                        map.put("y", String.valueOf(y));
+                        map.put("blue", String.valueOf(color[0]));
+                        map.put("green", String.valueOf(color[1]));
+                        map.put("red", String.valueOf(color[2]));
+                        result.add(map);
                     }
                 }
             }
         }
+
         Imgproc.drawContours(seedBuffer, Collections.singletonList(contour), 0,
                 new Scalar(0.0), Core.FILLED);
+
+        if (result.size() / (maxX - minX + 1.0) / (maxY - minY + 1.0) < threshold) {
+            result.clear();
+        }
+
+        return result;
+    }
+
+    private static void printSeedData(Map<String,String> data, List<Map<String,String>> seedData, PrintWriter writer) {
+        for (Map<String,String> map : seedData) {
+            for (String key : map.keySet()) {
+                data.put(key, map.get(key));
+                printMap(writer, data);
+            }
+        }
     }
 
     private static void printSeeds(Mat image, PrintWriter writer,
@@ -105,13 +120,16 @@ class SeedDataset {
         List<MatOfPoint> contours = Helper.getContours(image);
         Mat seedBuffer = Mat.zeros(image.rows(), image.cols(), CvType.CV_8UC1);
 
-        for (int i = 0; i < contours.size(); ++i) {
-            data.put("seed_number", String.valueOf(i));
-            MatOfPoint contour = contours.get(i);
+        int seedNumber = 0;
+        for (MatOfPoint contour : contours) {
             Double area = scale * Imgproc.contourArea(contour);
-            if (area < 50.0) {
-                data.put("area", area.toString());
-                printSingleSeed(contour, image, seedBuffer, writer, data);
+            if (area < 30.0 && area > 5.0) {
+                List<Map<String,String>> seedData = getSeedData(contour, image, seedBuffer, 0.5);
+                if (!seedData.isEmpty()) {
+                    data.put("seed_number", String.valueOf(seedNumber++));
+                    data.put("area", area.toString());
+                    printSeedData(data, seedData, writer);
+                }
             }
             contour.release();
         }
@@ -121,8 +139,8 @@ class SeedDataset {
 
     private static Mat filterByMask(Mat image, Mat mask) {
         Mat filtered = Helper.filterByMask(image, mask);
-        Range rows = new Range(filtered.rows() / 3, 2 * filtered.rows() / 3);
-        Range cols = new Range(filtered.cols() / 3, 2 * filtered.cols() / 3);
+        Range rows = new Range(filtered.rows() / 4, 3 * filtered.rows() / 4);
+        Range cols = new Range(filtered.cols() / 4, 3 * filtered.cols() / 4);
 
         Mat result = new Mat(filtered, rows, cols);
         filtered.release();
@@ -130,18 +148,16 @@ class SeedDataset {
         return result;
     }
 
-    private static Mat getMask(Mat image) {
+    private static Mat getMask(Mat image, Double scale) {
         Mat mask = Helper.binarizeSeed(image, SEED_TYPES);
-        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(50, 50));
-        Imgproc.morphologyEx(mask, mask, Imgproc.MORPH_CLOSE, kernel);
-
         Mat whiteMask = Helper.whiteThreshold(image);
-        kernel.release();
-        kernel = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(10, 10));
-        Imgproc.morphologyEx(whiteMask, whiteMask, Imgproc.MORPH_CLOSE, kernel);
-
         Core.bitwise_and(mask, whiteMask, mask);
         whiteMask.release();
+
+        int kernelSize = (int)(1.5 / Math.sqrt(scale));
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(kernelSize, kernelSize));
+        Imgproc.morphologyEx(mask, mask, Imgproc.MORPH_CLOSE, kernel);
+        Imgproc.morphologyEx(mask, mask, Imgproc.MORPH_OPEN, kernel);
         kernel.release();
 
         return mask;
@@ -195,9 +211,8 @@ class SeedDataset {
                 continue;
             }
 
-            // source color data
-            seedData.put("model", "Source");
-            Mat mask = getMask(image);
+            seedData.put("type", "source");
+            Mat mask = getMask(image, scale);
             Mat filtered = filterByMask(image, mask);
             mask.release();
             printSeeds(filtered, seedLog, seedData, scale);
@@ -207,10 +222,8 @@ class SeedDataset {
             extractedColorChecker.release();
             findColorChecker.fillColorChecker(calibrated, quad);
 
-            // calibrated color data
-            String name = model.getClass().getSimpleName();
-            seedData.put("model", name);
-            mask = getMask(calibrated);
+            seedData.put("type", "calibrated");
+            mask = getMask(calibrated, scale);
             filtered = filterByMask(calibrated, mask);
             calibrated.release();
             mask.release();
